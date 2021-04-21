1ad31856071e249ac472f3bfd5fc3bfe2be0de2a
Hamza Chowdhury <hhaammzzaa2@hotmail.co.uk>
2021-04-21 01:19:46 +0100

    Create basic user interface

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
 'use strict'
 
-console.log('hello world')
+function createPlayerAndRatingFields () {
+  const container = document.createElement('div')
+  container.className = 'player-div'
+
+  const playerField = document.createElement('input')
+  playerField.type = 'text'
+  playerField.className = 'player-field'
+
+  const ratingField = document.createElement('input')
+  ratingField.type = 'text'
+  ratingField.className = 'rating-field'
+  ratingField.placeholder = 5
+
+  container.appendChild(playerField)
+  container.appendChild(ratingField)
+  return container
+}
+
+for (let i = 0; i < 10; i++) {
+  const playersContainer = document.getElementsByClassName('players-container')[0]
+  playersContainer.appendChild(createPlayerAndRatingFields())
+}

bc97bc6d9837eea3b26baf3a9c3cf97e3e877e17
Hamza Chowdhury <hhaammzzaa2@hotmail.co.uk>
2021-04-22 16:58:40 +0100

    Rewrite team generating functionality

--- public/js/teams.js
+++ public/js/teams.js
@@ -1,36 +1,63 @@
 'use strict'
 
-const mockPlayerData = JSON.parse(`[
-  { "name": "hamza", "rating": "10" },
-  { "name": "mp", "rating": "1" },
-  { "name": "muj", "rating": "8" },
-  { "name": "ali", "rating": "9" },
-  { "name": "ibzy", "rating": "4" },
-  { "name": "kassir", "rating": "6" },
-  { "name": "biram", "rating": "4" },
-  { "name": "fahim", "rating": "5" },
-  { "name": "akram", "rating": "6" },
-  { "name": "dbsi", "rating": "8" }
-]`)
-
-function generateCombinations (numOfPlayers, numOfTeams, currentArray, result) {
-  if (currentArray.length === numOfPlayers) {
-    result.push(currentArray)
-    return currentArray
+function generateTeams (numOfPlayers, numOfTeams, playerData) {
+  const teams = []
+  for (let i = 0; i < numOfTeams; i++) teams.push([])
+  // randomly distribute players into teams
+  for (let i = 0; i < numOfPlayers; i++) {
+    const randomIdx = Math.floor(Math.random() * playerData.length)
+    teams[i % numOfTeams].push(playerData[randomIdx])
+    playerData.splice(randomIdx, 1)
   }
 
-  for (let i = 0; i < numOfTeams; i++) {
-    generateCombinations(numOfPlayers, numOfTeams, [...currentArray, i], result)
+  const games = []
+  // each iteration, swap a random player from highest rated team with random player from lowest rated team
+  for (let k = 0; k < numOfPlayers ** 2; k++) {
+    const ratings = teams.map((team) => {
+      return team.reduce((sum, player) => sum + +player.rating, 0)
+    })
+    // find index of team with highest rating, find team with lowest
+    const highestRatedTeamIdx = ratings.indexOf(Math.max(...ratings))
+    const lowestRatedTeamIdx = ratings.indexOf(Math.min(...ratings))
+    const highestRatedTeamRandomIdx = Math.floor(Math.random() * teams[highestRatedTeamIdx].length)
+    const lowestRatedTeamRandomIdx = Math.floor(Math.random() * teams[lowestRatedTeamIdx].length)
+    const randomPlayer1 = teams[highestRatedTeamIdx][highestRatedTeamRandomIdx]
+    const randomPlayer2 = teams[lowestRatedTeamIdx][lowestRatedTeamRandomIdx]
+    teams[highestRatedTeamIdx].splice(highestRatedTeamRandomIdx, 1)
+    teams[lowestRatedTeamIdx].splice(lowestRatedTeamRandomIdx, 1)
+    teams[highestRatedTeamIdx].push(randomPlayer2)
+    teams[lowestRatedTeamIdx].push(randomPlayer1)
+
+    const newRatings = teams.map((team) => {
+      return team.reduce((sum, player) => sum + +player.rating, 0)
+    })
+
+    const game = {
+      teams: JSON.parse(JSON.stringify(teams)),
+      ratings: newRatings,
+      ratingDifference: Math.max(...newRatings) - Math.min(...newRatings)
+    }
+
+    games.push(game)
   }
 
-  return result
+  let bestTeams = games[0]
+  for (const game of games) {
+    if (game.ratingDifference < bestTeams.ratingDifference) bestTeams = game
+  }
+
+  return bestTeams
 }
 
-function generatePossibleTeams (playerData, numOfTeams) {
-  const numOfPlayers = playerData.length
-  const combinations = generateCombinations(numOfPlayers, numOfTeams, [], [])
-
-  console.log(combinations)
-}
-
-// generatePossibleTeams(mockPlayerData, 2)
+// const mockPlayerData = JSON.parse(`[
+//   { "name": "hamza", "rating": "10" },
+//   { "name": "mp", "rating": "1" },
+//   { "name": "muj", "rating": "8" },
+//   { "name": "ali", "rating": "9" },
+//   { "name": "ibzy", "rating": "4" },
+//   { "name": "kassir", "rating": "6" },
+//   { "name": "biram", "rating": "4" },
+//   { "name": "fahim", "rating": "5" },
+//   { "name": "akram", "rating": "6" },
+//   { "name": "dbsi", "rating": "8" }
+// ]`)

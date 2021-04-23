c6a41351aa4ad6477227efd8efc87af0c32141f0
Hamza Chowdhury <hhaammzzaa2@hotmail.co.uk>
2021-04-23 18:11:06 +0100

    Remove unused code

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
